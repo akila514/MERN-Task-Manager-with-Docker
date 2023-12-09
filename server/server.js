6cf6e48b8f7eaa02a3304dd5524206078c89ea18
akila514 <akilajay514@gmail.com>
2023-12-10 04:17:16 +0530

    Added compose for dev and prod env

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hi There</h1>");
+  res.send("<h1>Hi There!</h1>");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
